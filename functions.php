3f06a779f378108bc894973a95f063fbec321d48
thracken <jeffgreenland.1@gmail.com>
2017-02-21 22:37:41 -0700

    Fixed date function.

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 /**/
 function date_or_modified_date(){
-  if ( the_date() == the_modified_date() ){
+  if ( get_the_date() == get_the_modified_date() ){
     return the_date();
   } else {
     return the_modified_date();

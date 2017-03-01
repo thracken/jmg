f147323a9ad9bee70753803f8fe33d01a1de3d74
Jeff <jeffgreenland.1@gmail.com>
2017-03-01 14:14:59 -0700

    Added floats for post title/date, removed colon from date function.

--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@
 function date_or_modified_date(){
   if ( get_the_date() == get_the_modified_date() ){
     $date = get_the_date();
-    echo("Published: " . $date);
+    echo("Published " . $date);
   } else {
     $date = get_the_modified_date();
-    echo("Modified: " . $date);
+    echo("Modified " . $date);
   }
 }
 
--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
   <div class="blog-post">
-  <h1><?php the_title(); ?> <small> <?php date_or_modified_date(); ?></small></h1>
-    <?php the_content(); ?>
+  <h1 class="clearfix"><span class="pull-left"><?php the_title(); ?></span> <small class="post-date pull-right"> <?php date_or_modified_date(); ?></small></h1>
+    <div>
+      <?php the_content(); ?>
+    </div>
   </div>
 
 <?php endwhile; else : ?>

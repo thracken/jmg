c9ba91c03604aacda26db38272f1fbb4c12ab6be
Jeff <jeffgreenland.1@gmail.com>
2017-03-01 14:40:32 -0700

    Added post-body class and styles, increase paragraph font size and adusted header tags in posts to stand out.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
   <div class="blog-post">
   <h1 class="clearfix"><span class="pull-left"><?php the_title(); ?></span> <small class="post-date pull-right"> <?php date_or_modified_date(); ?></small></h1>
-    <div>
+    <div class="post-body">
       <?php the_content(); ?>
     </div>
   </div>

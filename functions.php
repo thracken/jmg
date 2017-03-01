8a262ba32193b3965fe1622493ae8f4c76e5d6e3
Jeff <jeffgreenland.1@gmail.com>
2017-03-01 13:59:23 -0700

    Fixed post date function.

--- functions.php
+++ functions.php
@@ -3,11 +3,11 @@
 /**/
 function date_or_modified_date(){
   if ( get_the_date() == get_the_modified_date() ){
-    $date = the_date();
-    return "Published on: {$date}";
+    $date = get_the_date();
+    echo("Published: " . $date);
   } else {
-    $date = the_modified_date();
-    return "Last modified on: {$date}";
+    $date = get_the_modified_date();
+    echo("Modified: " . $date);
   }
 }
 

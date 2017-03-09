99d21c26e08b2a8d64593084dff870f37623fe0c
Jeff <jeffgreenland.1@gmail.com>
2017-03-09 10:42:23 -0700

    Added <hr> between blog posts w/adjusted styling, added some whitespace for smaller screens.

--- index.php
+++ index.php
@@ -3,16 +3,17 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
   <div class="blog-post row">
-  <div class="col-lg-2 col-xs-0"></div>
+  <div class="col-lg-2 col-xs-1"></div>
 
-  <div class="col-lg-8 col-xs-12">
+  <div class="col-lg-8 col-xs-10">
   <h1 class="clearfix"><span class="pull-left"><?php the_title(); ?></span> <small class="post-date pull-right"> <?php date_or_modified_date(); ?></small></h1>
     <div class="post-body">
       <?php the_content(); ?>
     </div>
+    <hr />
   </div>
 
-  <div class="col-lg-2 col-xs-0"></div>
+  <div class="col-lg-2 col-xs-1"></div>
   </div><!-- .blog-post row -->
 
 <?php endwhile; else : ?>

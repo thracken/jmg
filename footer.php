ad4bb4169b4f039407c8350db556a3adc40c9ad3
thracken <jeffgreenland.1@gmail.com>
2017-03-13 10:49:14 -0700

    Added footer area with styles and Wordpress widget area.

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
-    </div> <!-- #wrapper -->
-    <div id="footer">    
-        <?php wp_footer(); ?>
-    </div>
-  </body>
+</div> <!-- #wrapper -->
+<footer id="footer">
+  <div id="footer-widgets">
+    <?php dynamic_sidebar( 'footer-sideber' ); ?>
+    <?php wp_footer(); ?>
+  </div>
+</footer>
+</body>
 </html>
--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-/**/
+/*Custom date function for blog posts*/
 function date_or_modified_date(){
   if ( get_the_date() == get_the_modified_date() ){
     $date = get_the_date();
@@ -10,5 +10,18 @@ function date_or_modified_date(){
     echo("Modified " . $date);
   }
 }
+
+/*Adds footer widget area*/
+function footer_widget_init() {
+	register_sidebar( array(
+		'name'          => 'Footer Sidebar',
+		'id'            => 'footer-sideber',
+		'before_widget' => '<div class="footer-widget">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2>',
+		'after_title'   => '</h2>'
+	));
+}
+add_action( 'widgets_init', 'footer_widget_init' );
 
 ?>
